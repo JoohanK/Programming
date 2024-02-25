7623f529ef2d60544cdc9cca22f6f4a155f93301
JoohanK <94johkri1@skola.boras.se>
2024-02-25 18:02:28 +0100

    fix

--- banana-ufo/src/components/List/Athlets/AthleteList.jsx
+++ banana-ufo/src/components/List/Athlets/AthleteList.jsx
@@ -18,30 +18,52 @@ import { useState } from "react";
 
 export default AthletsList; */
 
-const AthleteList = ({ info }) => {
 
-    const [gold, setGold] = useState(info.gold);
-    const handleClick = () => {
-        setGold(gold + 1);
+
+
+const AthleteList = ({ info }) => {
+    const [gold, setGold] = useState(info);
+
+    const handleClick = (index) => {
+        setGold(prevGold => {
+            return prevGold.map((athlete, i) => {
+                if (i === index) {
+                    return { ...athlete, gold: athlete.gold + 1 };
+                }
+                return athlete;
+            });
+        });
+    }
+
+    const handleClickSub = (index) => {
+        setGold(prevGold => {
+            return prevGold.map((athlete, i) => {
+                if (i === index && athlete.gold > 0) {
+                    return { ...athlete, gold: athlete.gold - 1 };
+                }
+                return athlete;
+            });
+        });
     }
 
     return (
-        <>
         <div className="AthleteBox">
-        <h2>Athletes</h2>
-        {info.map((info) =>
-        <div className="AthleteCard">
-            <div className="AthleteInfo">
-        <p>Name: {info.name}</p>
-        <p>Gold Medals: {gold}</p> 
-        <button onClick={handleClick}>More Gold</button>
+            <h2>Athletes</h2>
+            {gold.map((athlete, index) => (
+                <div className="AthleteCard" key={index}>
+                    <div className="AthleteInfo">
+                        <p>Name: {athlete.name}</p>
+                        <p>Gold Medals: {athlete.gold}</p>
+                        <button onClick={() => handleClick(index)}>More Gold</button>
+                        <button onClick={() => handleClickSub(index)}>Less Gold</button>
+                    </div>
+                    {/* {info.img && <img src={info.img} alt={info.name} />} */}
+                    {athlete.img ? (<img src={athlete.img} alt={athlete.name} />) : (<img src="https://media.istockphoto.com/id/1158723576/de/vektor/running-mann-symbol-zeichen-flach.jpg?s=612x612&w=0&k=20&c=lqQ98SBe-Q3d0L6uoXp9kdiKMcirl46vb-FJskYS6yE=" alt="Default" />
+                    )}
+                </div>
+            ))}
         </div>
-        {/* {info.img && <img src={info.img} alt={info.name} />} */}
-        { info.img ? <img src={info.img} alt={info.name} /> : <img src="https://media.istockphoto.com/id/1158723576/de/vektor/running-mann-symbol-zeichen-flach.jpg?s=612x612&w=0&k=20&c=lqQ98SBe-Q3d0L6uoXp9kdiKMcirl46vb-FJskYS6yE="/>}
-        </div> 
-        )}
-        </div>
-        </>
-    )
-}
+    );
+};
+
 export default AthleteList;
